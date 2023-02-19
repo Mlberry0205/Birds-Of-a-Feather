2ab2808724ee8dad866d610e96ac04f9ce1e1f98
Morgan Berry <morganlyn.berry@gmail.com>
2023-02-19 13:41:16 -0700

    CSS and learn page refactor

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,15 @@
 import './Navbar.css'
-import logo from './bird-logo.png'
+import logo from './sounds.png'
 import { Link, NavLink } from 'react-router-dom';
-import '@fontsource/montez'
+// import '@fontsource/montez'
 
 
 const Navbar = () => {
   return (
     <nav className='navbar'>
-      <Link className='logo' to='/'>
-        {/* <img src={ logo } alt='Birds of a Feather logo' className='logo-img'/> */}
-        <h1 className='logo'>Birds of a Feather</h1>
+      <Link className='navbar-link' to='/'>
+        <img src={ logo } alt='Birds of a Feather logo' className='logo-img'/>
+        {/* <h1 className='logo'>Birds of a Feather</h1> */}
       </Link>
       <div className='nav-buttons' >
         <NavLink to="/Learn" className='about-us'>Learn</NavLink>
